40b273d0d9ed7033aafae0317b76227d98f55744
Herbert-Gao <59432783+Herbert-Gao@users.noreply.github.com>
2021-06-13 11:37:04 +0800

    Update Output.sql

--- MySQL_code/Output.sql
+++ MySQL_code/Output.sql
@@ -1,5 +1,5 @@
 select * from final_events
-into outfile 'G:/sql_8_database/Uploads/event_with_num.csv'
+into outfile 'G:/sql_8_database/Uploads/From_SQL.csv'
 fields terminated by ','
 optionally enclosed by '"'
 escaped by '"'
